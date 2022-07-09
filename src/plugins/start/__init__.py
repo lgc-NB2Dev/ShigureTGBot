d4f99df16f83b124981b7dff804323fd4ffe0193
student_2333 <lgc2333@126.com>
2022-07-10 02:15:56 +0800

    env

--- src/plugins/start/__init__.py
+++ src/plugins/start/__init__.py
@@ -1,15 +1,3 @@
-from nonebot import get_driver
+from .__main__ import *
 
-from .config import Config
-
-global_config = get_driver().config
-config = Config.parse_obj(global_config)
-
-# Export something for other plugin
-# export = nonebot.export()
-# export.foo = "bar"
-
-# @export.xxx
-# def some_function():
-#     pass
-
+__version__ = '0.1.0'
