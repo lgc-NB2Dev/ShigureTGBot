0f2a53830d5ff0944a295e60632cc605f60f5181
student_2333 <lgc2333@126.com>
2022-07-10 18:28:32 +0800

    update

--- src/plugins/about/__main__.py
+++ src/plugins/about/__main__.py
@@ -6,13 +6,15 @@ from ..base.cmd import on_command
 
 @on_command('about', '关于Bot').handle()
 async def _(matcher: Matcher, event: MessageEvent):
-    await matcher.send('关于Shigure\n'
-                       '-=-=-=-=-=-=-=-=-=-\n'
-                       '开发者：[@lgc2333](https://t.me/@lgc2333)\n'
-                       '开源地址：[Github](https://github.com/lgc2333/ShigureTGBot)'
-                       '使用框架：[Nonebot2](https://github.com/nonebot/nonebot2)\n'
-                       '特别感谢：\n'
-                       '  - [Wutzu](https://berthua.top/)（服务器提供）\n'
-                       '-=-=-=-=-=-=-=-=-=-\n'
-                       '[请我喝杯奶茶](https://t.me/stu2333_pd/50) | [一起聊天](https://t.me/stu2333_home)',
-                       reply_to_message_id=event.message_id)
+    await matcher.send(('关于Shigure\n'
+                        '-=-=-=-=-=-=-=-=-=-\n'
+                        '开发者：[@lgc2333](https://t.me/@lgc2333)\n'
+                        '开源地址：[Github](https://github.com/lgc2333/ShigureTGBot)'
+                        '使用框架：[Nonebot2](https://github.com/nonebot/nonebot2)\n'
+                        '特别感谢：\n'
+                        '  - [Wutzu](https://berthua.top/)（服务器提供）\n'
+                        '-=-=-=-=-=-=-=-=-=-\n'
+                        '[请我喝杯奶茶](https://t.me/stu2333_pd/50) | [一起聊天](https://t.me/stu2333_home)'
+                        ).replace('-', '\-'),
+                       reply_to_message_id=event.message_id,
+                       parse_mode='MarkdownV2')
--- src/plugins/base/cmd.py
+++ src/plugins/base/cmd.py
@@ -1,10 +1,12 @@
 """
 插件使用本插件注册命令，本插件自动将命令设置到Bot菜单
 """
-from nonebot import get_driver
+from nonebot import get_driver, logger
 from nonebot import on_command as raw_on_command
 from nonebot.adapters.telegram import Bot
+from nonebot.adapters.telegram.event import MessageEvent
 from nonebot.adapters.telegram.model import BotCommand
+from nonebot.matcher import Matcher
 
 command_list = []
 
@@ -14,6 +16,23 @@ def on_command(cmd: str | tuple, desc: str, *args, **kwargs):
     return raw_on_command(cmd, *args, **kwargs)
 
 
+def get_cmd_list_txt():
+    prefix = get_driver().config.command_start
+    return '\n'.join([f'{prefix}{x.command} - {x.description}' for x in command_list])
+
+
 @get_driver().on_bot_connect
 async def _(bot: Bot):
+    command_list.sort(key=lambda x: x.command)
+
+    logger.info(f'Bot {bot.self_id} connected')
+    logger.info(f'Registered Commands:\n{get_cmd_list_txt()}')
     await bot.set_my_commands(commands=command_list)
+
+
+@on_command('menu', '功能列表').handle()
+async def _(matcher: Matcher, event: MessageEvent):
+    await matcher.send('Shigure☆功能列表\n'
+                       '-=-=-=-=-=-=-=-=-=-\n'
+                       f'{get_cmd_list_txt()}',
+                       reply_to_message_id=event.message_id)
