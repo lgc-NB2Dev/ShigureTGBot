3459eee806f0f987b9a4c564323b7ff699e12d9e
lgc2333 <git>
2022-07-10 19:13:02 +0000

    Use Black to Format Code

--- src/plugins/netease/data_source.py
+++ src/plugins/netease/data_source.py
@@ -10,26 +10,27 @@ driver = get_driver()
 
 
 async def login():
-    phone = getattr(driver.config, 'netease_phone')
-    pwd = getattr(driver.config, 'netease_pwd')
+    phone = getattr(driver.config, "netease_phone")
+    pwd = getattr(driver.config, "netease_pwd")
 
     if (not phone) or (not pwd):
-        raise ValueError('请在env文件配置 NETEASE_PHONE 与 NETEASE_PWD')
+        raise ValueError("请在env文件配置 NETEASE_PHONE 与 NETEASE_PWD")
 
-    logger.info('开始登录网易云音乐')
+    logger.info("开始登录网易云音乐")
     try:
         ret = await wrapper.run(LoginViaCellphone, phone=phone, password=pwd)
-        nick = ret['content']['profile']['nickname']
+        nick = ret["content"]["profile"]["nickname"]
     except LoginFailedException as e:
-        logger.error('登录失败')
+        logger.error("登录失败")
         raise e
-    logger.info(f'欢迎您，{nick}')
+    logger.info(f"欢迎您，{nick}")
 
 
 async def search(name, limit=9, page=1, stype=SONG):
     offset = limit * (page - 1)
-    return await wrapper.run(GetSearchResult,
-                             name, stype=stype, limit=limit, offset=offset)
+    return await wrapper.run(
+        GetSearchResult, name, stype=stype, limit=limit, offset=offset
+    )
 
 
 async def get_track_info(ids: list):
@@ -37,5 +38,6 @@ async def get_track_info(ids: list):
 
 
 async def get_track_audio(song_ids: list, bit_rate=320000, encode_type="aac"):
-    return await wrapper.run(GetTrackAudio,
-                             song_ids, bitrate=bit_rate, encodeType=encode_type)
+    return await wrapper.run(
+        GetTrackAudio, song_ids, bitrate=bit_rate, encodeType=encode_type
+    )
