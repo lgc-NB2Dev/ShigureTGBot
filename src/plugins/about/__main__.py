6f30f7725838e985f4c03622b832264bd1f7a426
student_2333 <lgc2333@126.com>
2023-02-01 02:06:14 +0800

    sync

--- src/plugins/about/__main__.py
+++ src/plugins/about/__main__.py
@@ -2,7 +2,7 @@ from nonebot.adapters.telegram.event import MessageEvent
 from nonebot.matcher import Matcher
 
 from ..base.cmd import on_command
-from ..base.const import MD_LINE_SEP
+from ..base.const import LINE_SEP
 
 
 @on_command("about", "关于Bot").handle()
@@ -10,17 +10,19 @@ async def _(matcher: Matcher, event: MessageEvent):
     await matcher.send(
         (
             "关于Shigure\n"
-            f"{MD_LINE_SEP}\n"
-            "开发者：[@lgc2333](https://t.me/lgc2333)\n"
-            "开源地址：[Github](https://github.com/lgc2333/ShigureTGBot)\n"
-            "使用框架：[Nonebot2](https://github.com/nonebot/nonebot2)\n"
-            "特别感谢：\n"
-            "  \- [Wutzu](https://berthua.top/)（服务器提供）\n"
-            f"{MD_LINE_SEP}\n"
-            "[请我喝杯奶茶](https://t.me/stu2333_pd/50) \| "
-            "[一起聊天](https://t.me/stu2333_home)\n"
+            f"{LINE_SEP}\n"
+            '开发者：<a href="https://t.me/lgc2333">@lgc2333</a>\n'
+            '开源地址：<a href="https://github.com/lgc2333/ShigureTGBot">Github</a>\n'
+            '使用框架：<a href="https://github.com/nonebot/nonebot2">NoneBot2</a>\n'
+            "鸣谢：\n"
+            '  - <a href="https://github.com/MinatoAquaCrews/nonebot_plugin_crazy_thursday">'
+            'nonebot-plugin-crazy-thursday</a>\n'
+            '    疯狂星期四语录原插件\n'
+            f"{LINE_SEP}\n"
+            '<a href="https://t.me/stu2333_pd/50">请我喝杯奶茶</a> | '
+            '<a href="https://t.me/stu2333_home">一起聊天</a>'
         ),
         reply_to_message_id=event.message_id,
-        parse_mode="MarkdownV2",
+        parse_mode="HTML",
         disable_web_page_preview=True,
     )
