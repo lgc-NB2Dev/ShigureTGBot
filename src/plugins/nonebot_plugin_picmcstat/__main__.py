8c7a4344c244594aa00a29335d065d868a6cc7e1
student_2333 <lgc2333@126.com>
2023-02-01 01:51:54 +0800

    sync

---
+++ src/plugins/nonebot_plugin_picmcstat/__main__.py
@@ -0,0 +1,50 @@
+from typing import Awaitable, Callable, NoReturn
+
+from nonebot import on_regex, require
+from nonebot.internal.adapter import Message
+from nonebot.matcher import Matcher
+
+from ..base.cmd import CommandArg, on_command
+from .config import config
+
+require("nonebot_plugin_imageutils")
+
+from .draw import ServerType, draw
+
+motd_handler = on_command("motd", "获取 Minecraft 服务器信息", aliases={"!motd", "！motd"})
+
+
+@motd_handler.handle()
+async def _(matcher: Matcher, arg: Message = CommandArg()):
+    arg = arg.extract_plain_text()
+
+    svr_type: ServerType = "je"
+    be_svr_prefix = ["pe", "be"]
+    for p in be_svr_prefix:
+        if arg.startswith(p):
+            arg = arg.replace(p, "", 1)
+            svr_type = "be"
+            break
+
+    arg = arg.strip()
+    await matcher.finish(await draw(arg, svr_type))
+
+
+def get_shortcut_handler(
+    host: str, svr_type: ServerType
+) -> Callable[[...], Awaitable[NoReturn]]:
+    async def shortcut_handler(matcher: Matcher):
+        await matcher.finish(await draw(host, svr_type))
+
+    return shortcut_handler
+
+
+def startup():
+    if s := config.mcstat_shortcuts:
+        for shortcut in s:
+            on_regex(shortcut["regex"]).append_handler(
+                get_shortcut_handler(shortcut["host"], shortcut["type"])
+            )
+
+
+startup()
