82833af6f28a42d0db0a09356b4e7e55018f8e5e
student_2333 <lgc2333@126.com>
2023-05-17 02:45:48 +0800

    Update lrc_parser.py

--- src/plugins/netease/lrc_parser.py
+++ src/plugins/netease/lrc_parser.py
@@ -1,5 +1,9 @@
 import re
 from dataclasses import dataclass
+from enum import Enum
+from typing import Callable, List, Optional, TypeVar
+
+T = TypeVar("T")
 
 
 @dataclass
@@ -8,13 +12,27 @@ class LrcLine:
     """Lyric Time (ms)"""
     lrc: str
     """Lyric Content"""
+    skip_merge: bool = False
 
 
-LRC_TIME_REGEX = r"(?P<min>\d+):(?P<sec>\d+)([\.:](?P<mili>\d+))?"
+class LyricMatchMode(Enum):
+    EQUAL = 1
+    CLOSEST = 2
+
+
+LRC_TIME_REGEX = r"(?P<min>\d+):(?P<sec>\d+)([\.:](?P<mili>\d+))?(-(?P<meta>\d))?"
 LRC_LINE_REGEX = re.compile(rf"^((\[{LRC_TIME_REGEX}\])+)(?P<lrc>.*)$", re.M)
 
 
-def parse(lrc: str, ignore_empty: bool = True) -> list[LrcLine]:
+def find_last(array: List[T], predicate: Callable[[T], bool]) -> Optional[T]:
+    for i in range(len(array) - 1, -1, -1):
+        it = array[i]
+        if predicate(it):
+            return it
+    return None
+
+
+def parse(lrc: str, ignore_empty: bool = False) -> List[LrcLine]:
     parsed = []
     for line in re.finditer(LRC_LINE_REGEX, lrc):
         lrc = line["lrc"].strip().replace("\u3000", " ")
@@ -25,10 +43,10 @@ def parse(lrc: str, ignore_empty: bool = True) -> list[LrcLine]:
                 LrcLine(
                     time=(
                         int(i["min"]) * 60 * 1000
-                        + int(i["sec"]) * 1000
-                        + int(i["mili"] or 0)
+                        + int(float(f'{i["sec"]}.{i["mili"]}') * 1000)
                     ),
                     lrc=lrc,
+                    skip_merge=bool(i["meta"]) or lrc.startswith(("作词", "作曲", "编曲")),
                 )
                 for i in times
             ],
@@ -41,34 +59,38 @@ def parse(lrc: str, ignore_empty: bool = True) -> list[LrcLine]:
     return parsed
 
 
-def merge(*lrcs: list[LrcLine]) -> list[list[LrcLine]]:
-    lrcs = tuple(x.copy() for x in lrcs)
+def merge(*lyrics: List[LrcLine], threshold: int = 20) -> List[List[LrcLine]]:
+    lyrics = tuple(x.copy() for x in lyrics)
 
-    merged: list[list[LrcLine]] = []
-    last_time = 0
+    for lrc in lyrics:
+        while not lrc[-1].lrc:
+            lrc.pop()
 
-    main_lrc = lrcs[0]
-    sub_lrc = lrcs[1:]
+    main_lyric = lyrics[0]
+    sub_lyrics = lyrics[1:]
 
-    for line in main_lrc:
-        will_merge = [line]
-        last_time = line.time
+    for x in main_lyric:
+        if not x.lrc:
+            x.lrc = "--------"
 
-        for sub in sub_lrc:
-            index = None
+    merged: List[List[LrcLine]] = [[x] for x in main_lyric]
+    for merged_line in merged:
+        main_line = merged_line[0]
+        main_time = main_line.time
 
-            for i, lrc in enumerate(sub):
-                if lrc.time == last_time:
-                    index = i
+        for sub_lrc in sub_lyrics:
+            for i, line in enumerate(sub_lrc):
+                if (not line.lrc) or main_line.skip_merge:
+                    continue
+
+                if (main_time - threshold) <= line.time < (main_time + threshold):
+                    for _ in range(i + 1):
+                        it = sub_lrc.pop(0)
+                        if it.lrc:
+                            merged_line.append(it)
                     break
 
-            if index is not None:
-                for _ in range(index + 1):
-                    will_merge.append(sub.pop(0))
-
-        merged.append(will_merge)
-
-    for s in sub_lrc:
-        merged[-1].extend(s)
+    for sub_lrc in sub_lyrics:
+        merged[-1].extend(sub_lrc)
 
     return merged
